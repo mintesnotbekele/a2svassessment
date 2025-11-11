3c4f521c4671b8d6a4d52b750378b25e3062dbcd
Mintesnot Bekele <102607058+mintesnotbekele@users.noreply.github.com>
2025-11-12 00:37:03 +0300

    (Hot fix) hero section font update

--- src/components/organisms/HeroSection.tsx
+++ src/components/organisms/HeroSection.tsx
@@ -4,26 +4,23 @@ import { motion } from 'framer-motion';
 import Image from 'next/image';
 import HeroFood from '@/assets/heroFood.png'; 
 import MealSearchBar from '../atoms/MealSearchBar';
-// import MealSearchBar from '@/components/molecules/SearchBar/MealSearchBar';
 
 export const HeroSection: React.FC = () => {
   return (
     <section className="p-20 food-hero-bg relative bg-food-accent py-10 overflow-hidden">
       <div className="container mx-auto px-5 flex flex-col lg:flex-row items-center gap-12 relative z-10">
-        
-        {/* Left: Text + Search */}
         <div className="lg:flex-[0.6] flex-1 text-white z-10">
           <motion.h1
             style={{
               boxShadow: '0px 27px 82px 0px #FFAE0047, 0px 14px 15px 0px #FFAE0003',
-              fontFamily: 'Source Sans Pro',
+             
               fontWeight: 700,
               fontSize: '88px',
               lineHeight: '100%',
               letterSpacing: '0%',
               textAlign: 'left',
             }}
-            className="text-4xl md:text-5xl font-bold mb-3"
+            className="text-4xl md:text-5xl font-bold mb-3 font-primary"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
           >
@@ -50,9 +47,7 @@ export const HeroSection: React.FC = () => {
             }}
           />
         </div>
-
-      </div>
-      
+      </div>    
     </section>
   );
 };
